caf9245ee78ea511c0515c6e6b2e622dd9a74df3
LeeSeoTaik <58031119+LeeSeoTaik@users.noreply.github.com>
2020-02-18 12:33:30 +0900

    Add files via upload

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+ "fmt"
+
+ "github.com/LeeSeoTaik/golanguage"
+)
+
+func main() {
+ //reverse a sentense.
+ fmt.Println(Utils.Reverse("HTAPOGgnitteS/ikiw/og/gnalog/moc.buhtig//:sptth tlusnoc dluohs sresu swodniW"))
+}
